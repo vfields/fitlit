81a11fd65081f0e32e0bf776eec8dce67d7fc3ca
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-27 15:35:25 -0600

    Refactor unit tests

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,16 +1,58 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
-import userData from '../src/data/users';
 
 describe('User Repository', () => {
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
 
+  let userData;
   let userRepo;
 
   beforeEach(() => {
-    userRepo = new UserRepository(userData);
+    userData = [
+     {
+       "id": 1,
+       "name": "Tori Fields",
+       "address": "426 Cordova Ave NW",
+       "email": "victoriaashleyfields@gmail.com",
+       "strideLength": 4.3,
+       "dailyStepGoal": 10000,
+       "friends": [
+         16,
+         4,
+         8
+       ]
+     },
+     {
+       "id": 2,
+       "name": "Hazel Pablo",
+       "address": "A different address",
+       "email": "hazel@gmail.com",
+       "strideLength": 4.5,
+       "dailyStepGoal": 7500,
+       "friends": [
+         9,
+         18,
+         24,
+         19
+       ]
+     },
+     {
+       "id": 3,
+       "name": "Matt Press",
+       "address": "another different address",
+       "email": "matt@yahoo.com",
+       "strideLength": 4.4,
+       "dailyStepGoal": 5000,
+       "friends": [
+         19,
+         11,
+         42,
+         33
+       ]
+     }];
+     userRepo = new UserRepository(userData);
   });
 
   it ('should be an instance of UserRepository', () => {
@@ -21,29 +63,12 @@ describe('User Repository', () => {
     expect(userRepo.data).to.deep.equal(userData);
   });
 
-  it.skip ('should be able to find user data given a user ID', () => {
-    console.log('findUserData(1)', userRepo.findUserData(1));
-    console.log('userData[0]', userData[0]);
-
-    console.log('findUserData(2)', userRepo.findUserData(2));
-    console.log('userData[1]', userData[1]);
-
-    // expect(userRepo.findUserData(1)).to.equal(userData[0]); throws a webpack error...
+  it ('should be able to find user data given a user ID', () => {
+    expect(userRepo.findUserData(1)).to.equal(userData[0]);
+    expect(userRepo.findUserData(2)).to.equal(userData[1]);
   });
 
   it ('should be able to calculate the average step goal amongst all users', () => {
-    const average = userData
-      .map(user => user.dailyStepGoal)
-      .reduce((acc, stepGoal, index, userDataArray) => {
-        if (index === userDataArray.length - 1) {
-          return (acc + stepGoal) / userDataArray.length;
-        }
-        acc = acc + stepGoal;
-        return acc;
-      }, 0);
-
-      // not sure if that's the right way to go about testing this?
-
-    expect(userRepo.calculateAvgStepGoal()).to.equal(average);
+    expect(userRepo.calculateAvgStepGoal()).to.equal(7500);
   })
 });
