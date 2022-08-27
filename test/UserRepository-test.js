0ca74e7e2e28c67c857d5c0eac26fea083cb3d32
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-27 11:13:31 -0600

    Create passing test for calculate avg goal, bug in test for find user data given id

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -7,7 +7,31 @@ describe('User Repository', () => {
     expect(UserRepository).to.be.a('function');
   });
 
+  let userRepo;
+
+  beforeEach(() => {
+    userRepo = new UserRepository(userData);
+  });
+
+  it ('should be an instance of UserRepository', () => {
+    expect(userRepo).to.be.an.instanceOf(UserRepository);
+  });
+
   it ('should store user data', () => {
-    console.log(userData);
+    expect(userRepo.data).to.deep.equal(userData);
+  });
+
+  it ('should be able to find user data given a user ID', () => {
+    console.log('findUserData(1)', userRepo.findUserData(1));
+    console.log('userData[0]', userData[0]);
+
+    console.log('findUserData(2)', userRepo.findUserData(2));
+    console.log('userData[1]', userData[1]);
+
+    // expect(userRepo.findUserData(1)).to.equal(userData[0]); throws a webpack error...
+  });
+
+  it ('should be able to calculate the average step goal amongst all users', () => {
+    expect(userRepo.calculateAvgStepGoal()).to.equal(6700)
   })
 });
