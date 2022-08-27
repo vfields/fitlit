44ac4bcd414abc398c12ba8877745f01d7578047
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-27 10:58:52 -0600

    Import user data to User Repository test file

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,13 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import userData from '../src/data/users';
 
 describe('User Repository', () => {
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
+
+  it ('should store user data', () => {
+    console.log(userData);
+  })
 });
