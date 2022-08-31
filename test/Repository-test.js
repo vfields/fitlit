be8eb75bf14e674107e28b12a8e00d13ecf03ebb
Hunter Monroe <huntis@outlook.com>
2022-08-31 11:12:38 -0600

    Add user mock data

--- test/Repository-test.js
+++ test/Repository-test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import Repository from '../src/Repository';
 import {hydrationMockData } from '../src/data/hydrationData';
+import userData from '../src/data/userData';
 
 
 describe('Repository', () => {
@@ -8,56 +9,12 @@ describe('Repository', () => {
     expect(Repository).to.be.a('function');
   });
 
-  let userData;
   let userRepo;
-  let hydrationData;
   let hydrationRepo; 
 
   beforeEach(() => {
-    userData = [
-     {
-       "id": 1,
-       "name": "Tori Fields",
-       "address": "426 Cordova Ave NW",
-       "email": "victoriaashleyfields@gmail.com",
-       "strideLength": 4.3,
-       "dailyStepGoal": 10000,
-       "friends": [
-         16,
-         4,
-         8
-       ]
-     },
-     {
-       "id": 2,
-       "name": "Hazel Pablo",
-       "address": "A different address",
-       "email": "hazel@gmail.com",
-       "strideLength": 4.5,
-       "dailyStepGoal": 7500,
-       "friends": [
-         9,
-         18,
-         24,
-         19
-       ]
-     },
-     {
-       "id": 3,
-       "name": "Matt Press",
-       "address": "another different address",
-       "email": "matt@yahoo.com",
-       "strideLength": 4.4,
-       "dailyStepGoal": 5000,
-       "friends": [
-         19,
-         11,
-         42,
-         33
-       ]
-     }];
      userRepo = new Repository(userData);
-     hydrationRepo = new Repository()
+     hydrationRepo = new Repository(hydrationMockData)
   });
 
   it ('should be an instance of Repository', () => {
