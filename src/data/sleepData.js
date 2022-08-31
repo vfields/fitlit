a2635e2eb63df66c9c359ecbb89b8e9fc63ff907
MatthewPress <press.matt14@gmail.com>
2022-08-31 16:43:44 -0600

    test: add sleep data for the user class tests

---
+++ src/data/sleepData.js
@@ -0,0 +1,9 @@
+export const sleepData = [
+  {userID: 1, date: '2019/06/15', hoursSlept: 7, sleepQuality: 4.7},
+  {userID: 1, date: '2019/06/16', hoursSlept: 8, sleepQuality: 4.7},
+  {userID: 1, date: '2019/06/17', hoursSlept: 9, sleepQuality: 4.7},
+  {userID: 1, date: '2019/06/18', hoursSlept: 10, sleepQuality: 4.7},
+  {userID: 1, date: '2019/06/19', hoursSlept: 11, sleepQuality: 4.7},
+  {userID: 1, date: '2019/06/20', hoursSlept: 12, sleepQuality: 4.7},
+  {userID: 1, date: '2019/06/21', hoursSlept: 13, sleepQuality: 4.7},
+];
