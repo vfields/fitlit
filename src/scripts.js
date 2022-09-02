b7aa9a364de245a1932e686aeecb495b8c8264a5
Hunter Monroe <huntis@outlook.com>
2022-09-02 13:37:45 -0600

    Refactor/ code clean up

--- src/scripts.js
+++ src/scripts.js
@@ -1,28 +1,19 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // DEPENDENCIES **************************************************
 import './css/styles.css';
-import './images/turing-logo.png';
-// import userData from './data/users';
 import fetchData from './apiCalls';
 import Repository from './Repository';
 import User from './User';
 
 // GLOBAL DATA ***************************************************
 let userRepository;
-let randomUser;
 let hydrationRepository;
-let timeframe;
 let sleepRepository;
+let randomUser;
+let timeframe;
 
 // FETCH DATA *****************************************************
-// const usersPromise = fetchData('https://fitlit-api.herokuapp.com/api/v1/users');
-// const hydrationPromise = fetchData('https://fitlit-api.herokuapp.com/api/v1/hydration');
-
 Promise.all([fetchData("users"), fetchData("hydration"), fetchData("sleep")])
   .then((repos) => {
-    console.log(repos);
     setData(repos);
   });
 
@@ -31,16 +22,16 @@ function setData(repos) {
   randomUser = getRandomUser(userRepository.data);
   hydrationRepository = new Repository(repos[1].hydrationData);
   randomUser.setUserData(hydrationRepository, 'hydrationData', 'userID');
-  timeframe = randomUser.hydrationData[randomUser.hydrationData.length - 1].date;
   sleepRepository = new Repository(repos[2].sleepData);
   randomUser.setUserData(sleepRepository, 'sleepData', 'userID');
+  timeframe = randomUser.hydrationData[randomUser.hydrationData.length - 1].date;
+ 
   displayUserData();
 }
 
 function getRandomUser(users) {
   const randomIndex = Math.floor(Math.random() * users.length);
   const randomUserData = userRepository.findUser(randomIndex, 'id');
-  console.log("random user data", randomUserData);
   return new User(randomUserData[0]);
 }
 
@@ -53,22 +44,21 @@ const repoStepGoal = document.querySelector(".repo-step-goal");
 const userStrideLength = document.querySelector(".user-stride-length");
 const userFriends = document.querySelector(".friend-names");
 const timeframeDisplay = document.querySelector(".timeframe-display");
+const timeFrameBtn = document.querySelector(".timeframe-button");
 const timeframeButtonText = document.querySelector(".timeframe-button-text");
+const waterInfo = document.querySelector(".water-information");
 const avgWaterAmount = document.querySelector(".avg-water-amount");
 const waterDate = document.querySelector(".water-date");
 const waterAmount = document.querySelector(".water-amount");
+const sleepInfo = document.querySelector(".sleep-information")
 const avgSleepAmount = document.querySelector(".avg-sleep-amount");
 const sleepDate = document.querySelector(".sleep-date");
 const sleepAmount = document.querySelector(".sleep-amount");
 const avgSleepQuality = document.querySelector(".avg-sleep-quality");
 const sleepQual = document.querySelector(".sleep-quality");
-const waterInfo = document.querySelector(".water-information");
-const timeFrameBtn = document.querySelector(".timeframe-button");
-const sleepInfo = document.querySelector(".sleep-information")
 
 
 // EVENT LISTENERS ************************************************
-
 timeFrameBtn.addEventListener('click', displayWeeklyTimeFrames);
 
 // EVENT HANDLERS *************************************************
@@ -79,18 +69,13 @@ function displayUserData() {
 }
 
 function displayUserInfo() {
-  console.log("friends", )
   userFirstName.innerText = randomUser.name;
   userAddress.innerText = randomUser.address;
   userEmail.innerText = randomUser.email;
   const theirFriends = randomUser.friends.flatMap(friend => userRepository.findUser(friend, 'id'));
-  const theirFriendsSteps = theirFriends.map(foe => foe.dailyStepGoal);
-  const theirFriendsName = theirFriends.map(foe => foe.name).forEach((friend) => {
+  const theirFriendsName = theirFriends.map(friend => friend.name).forEach((friend) => {
     userFriends.innerHTML += `<p>${friend}</p>`
   });
-
-
-
 }
 
 function displayStepData() {
@@ -110,26 +95,37 @@ function displaySleepData() {
   sleepDate.innerText = timeframe;
   sleepAmount.innerText = randomUser.sleepData[randomUser.sleepData.length - 1].hoursSlept;
   sleepQual.innerText = randomUser.sleepData[randomUser.sleepData.length - 1].sleepQuality;
-  sleepInfo.innerHTML += `<p><span class="sleep-date">${timeframe}</span>
-  : <span class="sleep-amount">${randomUser.sleepData[randomUser.sleepData.length - 1].hoursSlept}</span> hrs,
-  <span class="sleep-quality"> ${randomUser.sleepData[randomUser.sleepData.length - 1].sleepQuality}</span>/5 Quality</p>`;
+  sleepInfo.innerHTML += `
+  <p>
+   <span class="sleep-date">${timeframe}</span>:
+   <span class="sleep-amount">${randomUser.sleepData[randomUser.sleepData.length - 1].hoursSlept}</span> hrs,
+   <span class="sleep-quality"> ${randomUser.sleepData[randomUser.sleepData.length - 1].sleepQuality}</span>/5 Quality
+  </p>
+   `;
 }
 
 function displayHydrationData() {
   waterDate.innerText = timeframe;
   waterAmount.innerText = randomUser.hydrationData[randomUser.hydrationData.length - 1].numOunces;
-  waterInfo.innerHTML += `<p><span class="water-date">
-  ${timeframe}</span>
-   : <span class="water-amount">${randomUser.hydrationData[randomUser.hydrationData.length - 1].numOunces}</span> oz</p>`;
+  waterInfo.innerHTML += `
+  <p>
+   <span class="water-date">${timeframe}</span>: 
+   <span class="water-amount">${randomUser.hydrationData[randomUser.hydrationData.length - 1].numOunces}</span> oz
+  </p>
+   `;
 }
 
 function displaySleepWeek() {
   let displaySleepDays = randomUser.getUserWeeklyData(randomUser.sleepData[randomUser.sleepData.length - 7].date,
     randomUser.sleepData[randomUser.sleepData.length - 1].date, 'sleepData');
   displaySleepDays.forEach((element) => {
-    sleepInfo.innerHTML += `<p><span class="sleep-date">${element.date}</span>
-          : <span class="sleep-amount">${element.hoursSlept}</span> hrs,
-          <span class="sleep-quality"> ${element.sleepQuality}</span>/5 Quality</p>`;
+  sleepInfo.innerHTML += `
+  <p>
+    <span class="sleep-date">${element.date}</span>: 
+    <span class="sleep-amount">${element.hoursSlept}</span> hrs,
+    <span class="sleep-quality"> ${element.sleepQuality}</span>/5 Quality
+  </p>
+    `;
   });
 }
 
@@ -137,16 +133,20 @@ function displayHyrationWeek() {
   let displayHydrationDays = randomUser.getUserWeeklyData(randomUser.hydrationData[randomUser.hydrationData.length - 7].date,
     randomUser.hydrationData[randomUser.hydrationData.length - 1].date, 'hydrationData');
   displayHydrationDays.forEach((element) => {
-    waterInfo.innerHTML += `<p><span class="water-date">${element.date}</span>
-    : <span class="water-amount">${element.numOunces}</span> oz</p>`;
+    waterInfo.innerHTML += `
+  <p>
+    <span class="water-date">${element.date}</span>: 
+    <span class="water-amount">${element.numOunces}</span> oz
+  </p>
+    `;
   });
 }
 
 function setTimeframeDisplays() {
   waterInfo.innerHTML = "";
   sleepInfo.innerHTML = "";
+  
   if (timeframe === randomUser.hydrationData[randomUser.hydrationData.length - 1].date) {
-    console.log('IF')
     timeframeDisplay.innerText = timeframe;
     timeframeButtonText.innerText = "WEEKLY";
     displaySleepData();
