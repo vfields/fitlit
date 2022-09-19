51aa987198205f8cd3f0fa3255ff4c78d21a1f53
MatthewPress <press.matt14@gmail.com>
2022-09-18 23:23:42 -0600

    chore: apply turing js style guide to api call functions"

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,33 +4,34 @@ function fetchData(repo) {
   return fetch(`http://localhost:3001/api/v1/${repo}`)
       .then(response => {
         if (response.ok) {
-        return response.json();
-         }
-      throw new Error('Not a 200 status');
-     })
-       .catch(error => {
-       alert('Oops, something went wrong. Try refreshing your page.');
-       })
-};
+          return response.json();
+        }
+        throw new Error('Not a 200 status');
+      })
+      .catch(error => {
+        alert('Oops, something went wrong. Try refreshing your page.');
+      });
+}
 
 function postData(repo, userData) {
   const requestData = {
       method:'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(userData),
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(userData)
     };
+
     return fetch(`http://localhost:3001/api/v1/${repo}`, requestData)
       .then(response => {
         if (response.ok) {
-        alert('Information submitted')
-        displayDataForm()
-        return response.json();
-         }
-      throw new Error('Not a 200 status');
-     })
-      .catch(error => {
-      alert('Oops, something went wrong. Try again later')
+          alert('Information submitted')
+          displayDataForm()
+          return response.json();
+        }
+        throw new Error('Not a 200 status');
       })
-};
+      .catch(error => {
+        alert('Oops, something went wrong. Try again later');
+      });
+}
 
-export { fetchData, postData }
+export { fetchData, postData };
