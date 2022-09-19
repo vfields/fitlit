96a3556f47e1016145dde35024ab3296a6adb935
MatthewPress <press.matt14@gmail.com>
2022-09-18 21:47:29 -0600

    chore: remove unnecessary spaces

--- test/Repository-test.js
+++ test/Repository-test.js
@@ -20,16 +20,16 @@ describe('Repository', () => {
      activityRepo = new Repository(activityData);
   });
 
-  it ('should be an instance of Repository', () => {
+  it('should be an instance of Repository', () => {
     expect(userRepo).to.be.an.instanceOf(Repository);
   });
 
-  it ('should store any repo data', () => {
+  it('should store any repo data', () => {
     expect(userRepo.data).to.deep.equal(userData);
     expect(hydrationRepo.data).to.deep.equal(hydrationData);
   });
 
-  it ('should be able to find user data given a user ID', () => {
+  it('should be able to find user data given a user ID', () => {
     const user1 = userRepo.findUser(1 ,'id');
     const user2 = userRepo.findUser(2, 'id');
     const user3 = hydrationRepo.findUser(1, 'userID');
@@ -39,7 +39,7 @@ describe('Repository', () => {
     expect(user3[0]).to.equal(hydrationData[0]);
   });
 
-  it ('should be able to calculate the average of a given property amongst all users', () => {
+  it('should be able to calculate the average of a given property amongst all users', () => {
     const stepGoalAvg = userRepo.calcRepoAvg('dailyStepGoal');
     const numOuncesAvg = hydrationRepo.calcRepoAvg('numOunces');
 
